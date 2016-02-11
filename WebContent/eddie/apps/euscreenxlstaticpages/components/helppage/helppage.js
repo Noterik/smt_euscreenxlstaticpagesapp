91e7c63169362642265cfa2f537a3718d3764e9c
Petar Stoyanov <petar@noterik.nl>
2016-02-11 16:45:49 +0200

    added p tag and class toggle function

--- WebContent/eddie/apps/euscreenxlstaticpages/components/helppage/helppage.js
+++ WebContent/eddie/apps/euscreenxlstaticpages/components/helppage/helppage.js
@@ -1,5 +1,15 @@
-var Termspage = function () {
+var Helppage = function () {
     Component.apply(this, arguments);
+    
+//  when you click on the anchor tag we remove the "showtext" class
+    $(".triggers").click(function() {
+	  $(".play").each(function() {
+		  $(".play").removeClass("showtext");
+	  });
+	  // we add the "showtext" class only for the clicked element
+	  $(this).find('.play').addClass("showtext");
+	});
+    
 };
 
-Termspage.prototype = Object.create(Component.prototype);
+Helppage.prototype = Object.create(Component.prototype);

56f2daed1e9975ae059825677628d571f9856dc6
Petar Stoyanov <petar@noterik.nl>
2016-02-15 17:02:00 +0200

    updated help page

--- WebContent/eddie/apps/euscreenxlstaticpages/components/helppage/helppage.js
+++ WebContent/eddie/apps/euscreenxlstaticpages/components/helppage/helppage.js
@@ -6,9 +6,21 @@ var Helppage = function () {
 	  $(".play").each(function() {
 		  $(".play").removeClass("showtext");
 	  });
+		  
 	  // we add the "showtext" class only for the clicked element
 	  $(this).find('.play').addClass("showtext");
 	});
+    // function for the url changes for pc
+    $(".triggers").on("click",function(){
+        var urlChanging = $(this).attr("youtube-data");
+        $("#testnew").attr("src", urlChanging);
+    });
+    
+    $(".triggers").bind("tap",function(){
+        var urlChanging = $(this).attr("youtube-data");
+        $("#testnew").attr("src", urlChanging);
+    });
+    
     
 };
 
